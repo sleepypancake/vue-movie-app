eeb24356319603d2c1f8c2a41d6c8ee7c7497e41
Anastasia Brikun <brikun.anastasya@yandex.ru>
2021-04-01 12:15:08 +0300

    Starting movie app blocks

---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default {
+    apikey: "247a3f6c"
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591">
+        <img src="https://animego.org/upload/anime/images/5a3fbf0c1fd3c.jpg" 
+        alt="Наруто" class="featured-img">
+        <div class="detail">
+          <h3>Наруто</h3>
+          <p> Наруто грезит всю жизнь – стать Х
+             окагэ – вождем деревни, что скрыта в листве. Много соперников встретит юнец, но это путь ниндзя! Путь Узумаки! Путь настоящего Хокагэ!</p>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
+
 </script>
+
+<style lang="scss">
+ .home {
+   .feature-card {
+     position: relative;
+   }
+
+   .featured-img {
+     display: block;
+     width: 100%;
+     height: 300px;
+     object-fit: cover;
+     position: relative;
+     z-index: 0;
+   }
+
+   .detail {
+     position: absolute;
+     left: 0;
+     right: 0;
+     bottom: 0;
+     background-color: rgba(0, 0, 0, 0.6);
+     padding: 16px;
+     z-index: 1;
+   }
+
+   h3 {
+     color: #fff;
+     margin-bottom: 16px;
+   }
+
+   p {
+     color: #fff;
+   }
+ }
+</style>
